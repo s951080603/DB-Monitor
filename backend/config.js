4bd2f0b1ddf4f7f24e05f2b9fb1ae9d3d93d6835
Louis Chiu <s951080603@gmail.com>
2023-06-20 19:13:50 +0800

    Remove config file

--- backend/config.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require("pg");
-module.exports.client = new Client({
-  user: "postgres",
-  host: "epa.clrt814nnnpd.ap-southeast-1.rds.amazonaws.com",
-  database: "epa",
-  password: "#Yzu70936",
-  port: 5432,
-});
